66637b212617c4cd4cbe97f96b3cb15ab9c075eb
possycrypto <florin.mi423@outlook.com>
2024-03-17 04:46:27 -0700

    modified:   01_hello_openai.py

--- 01_hello_openai.py
+++ 01_hello_openai.py
@@ -1,10 +1,15 @@
+
+
+
 from openai import OpenAI;
 client = OpenAI()
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."}
+        {
+            "role": "system", 
+            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."}
     ]
 )
 
